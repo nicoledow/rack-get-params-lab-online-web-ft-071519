e78d0362b1b480111c59cd8a96ce1429dd612992
Nicole Dow <nikki.dow71@gmail.com>
2019-08-25 19:31:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,8 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
      if @@cart.length > 0 
-       
+       @@cart.each do |item|
+         resp.write "#{item}\n"
      end
     else
       resp.write "Path Not Found"
