fda4f2606c1989a59f0781c47cfa2325a03601bd
Nicole Dow <nikki.dow71@gmail.com>
2019-08-25 19:36:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,8 @@ class Application
       if @@items.include?(item_to_add)
         @@cart << item_to_add
         resp.write "Added #{item_to_add}"
+      else
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
