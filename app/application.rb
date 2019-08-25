aad746c2a41e8646e0ec473c4d034345f43d7499
Nicole Dow <nikki.dow71@gmail.com>
2019-08-25 19:28:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        
+      end
     else
       resp.write "Path Not Found"
     end
